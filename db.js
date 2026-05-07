591be89b2199b49bc2a0eaceb6588f16b50c0065
anirudhbhat777 <anirudhbhatofficial30@gmail.com>
2026-05-07 21:00:19 +0530

    Refactor db.js to simplify database connection
    Removed redundant pool configuration and export.

--- db.js
+++ db.js
@@ -5,15 +5,6 @@ const pool = new Pool({
   ssl: {
     rejectUnauthorized: false,
   },
-});
-
-module.exports = pool;
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+})
 
 module.exports = pool;
